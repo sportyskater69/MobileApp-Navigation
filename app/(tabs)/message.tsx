cff0adf5c1c0e65d08c0f311b375a9d0a8d4aadb
sportyskater69 <rowan.feland@edu.sait.ca>
2026-02-19 19:43:39 -0700

    Created reusable search bar and finished the message page

--- app/(tabs)/message.tsx
+++ app/(tabs)/message.tsx
@@ -1,10 +1,11 @@
 import { View } from "react-native";
+import Messages from "../message_header";
 
 
 export default function Message() {
     return (
         <View style={{ flex: 1, backgroundColor: "#ffff" }}>
-
+            <Messages />
         </View>
     );
 }
