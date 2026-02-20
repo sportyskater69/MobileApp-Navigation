566e1512fcc87703c294654337c8b2cc75e2d5f1
zeynepcamgoz <Zeynep.Camgoz@edu.sait.ca>
2026-02-19 20:56:35 -0700

    Finished search tab

--- app/search_bar.tsx
+++ app/search_bar.tsx
@@ -2,29 +2,32 @@ import { Image, TextInput, View } from "react-native";
 
 export default function SearchBar() {
     return (
-        <View
-            style={{
-                flexDirection: "row",
-                alignItems: "center",
-                backgroundColor: "#EFEFEF",
-                borderRadius: 20,
-                marginHorizontal: 15,
-                marginVertical: 10,
-                paddingHorizontal: 10,
-                height: 40,
-            }}
-        >
-            <Image
-                source={require("../assets/images/Search-Icon.png")}
-                style={{ width: 18, height: 18, marginRight: 8, opacity: 0.6 }}
-            />
+        <View>
+            <View
+                style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    backgroundColor: "#EFEFEF",
+                    borderRadius: 20,
+                    marginHorizontal: 15,
+                    marginTop: 40,
+                    marginVertical: 10,
+                    paddingHorizontal: 10,
+                    height: 40,
+                }}
+            >
+                <Image
+                    source={require("../assets/images/Search-Icon.png")}
+                    style={{ width: 25, height: 25, marginRight: 8, opacity: 0.6 }}
+                />
 
-            <TextInput
-                placeholder="Search or ask Meta AI"
-                placeholderTextColor="#8e8e8e"
-                style={{ flex: 1 }}
-                editable={false}
-            />
+                <TextInput
+                    placeholder="Search or ask Meta AI"
+                    placeholderTextColor="#8e8e8e"
+                    style={{ flex: 1 }}
+                    editable={false}
+                />
+            </View>
         </View>
     );
 }
--- app/search_component.tsx
+++ app/search_component.tsx
@@ -1,18 +1,91 @@
-import { Image, Pressable, Text, View } from "react-native";
+import { Dimensions, Image, Pressable, Text, View } from "react-native";
+import SearchBar from "./search_bar";
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 export default function Searcher() {
   return (
     <View>
-      <Pressable onPress={() => { }}>
-        <Text
-          style={{
-            marginTop: 40, fontSize: 15, marginLeft: 24
-          }}>
-          Search with Meta AI</Text>
-      </Pressable>
+      <SearchBar />
+      <View style={{ flexDirection: "row" }}>
+        <View style={{
+          marginLeft: 20,
+          marginTop: 5,
+          backgroundColor: "#040000",
+          borderRadius: 10,
+          paddingBottom: 8,
+          paddingTop: 8,
+          paddingRight: 9,
+          paddingLeft: 9,
+        }}>
+          <Pressable>
+            <Text style={{
+              color: "#ffffff",
+              fontWeight: "bold"
+            }}>For you</Text>
+          </Pressable>
+        </View>
+        <View style={{
+          marginLeft: 10,
+          marginTop: 5,
+          borderRadius: 10,
+          paddingBottom: 8,
+          paddingTop: 8,
+          paddingRight: 9,
+          paddingLeft: 9,
+          backgroundColor: "#EFEFEF",
+        }}>
+          <Pressable>
+            <Text style={{ fontWeight: 'bold' }}>Memes</Text>
+          </Pressable>
+        </View>
+        <View style={{
+          marginLeft: 10,
+          marginTop: 5,
+          borderRadius: 10,
+          paddingBottom: 8,
+          paddingTop: 8,
+          paddingRight: 9,
+          paddingLeft: 9,
+          backgroundColor: "#EFEFEF",
+        }}>
+          <Pressable>
+            <Text style={{ fontWeight: 'bold' }}>Foodie</Text>
+          </Pressable>
+        </View>
+        <View style={{
+          marginLeft: 10,
+          marginTop: 5,
+          borderRadius: 10,
+          paddingBottom: 8,
+          paddingTop: 8,
+          paddingRight: 9,
+          paddingLeft: 9,
+          backgroundColor: "#EFEFEF",
+        }}>
+          <Pressable>
+            <Text style={{ fontWeight: 'bold' }}>Fitness Routine</Text>
+          </Pressable>
+        </View>
+        <View style={{
+          marginLeft: 10,
+          marginTop: 5,
+          borderRadius: 10,
+          paddingBottom: 8,
+          paddingTop: 8,
+          paddingRight: 9,
+          paddingLeft: 9,
+          backgroundColor: "#EFEFEF",
+        }}>
+          <Pressable>
+            <Text style={{ fontWeight: 'bold' }}>Travel</Text>
+          </Pressable>
+        </View>
+      </View>
       <View>
-        <Image source={require("../assets/images/profile-photo.png")}
-          style={{ height: 25, width: 25, marginLeft: 10 }} />
+        <Image source={require("../assets/images/search-feed.jpeg")}
+          style={{ height: screenHeight, width: screenWidth, marginTop: 5 }} />
       </View>
     </View>
   );
